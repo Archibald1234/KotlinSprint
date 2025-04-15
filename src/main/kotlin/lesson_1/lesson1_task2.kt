a1275f6ea94f72bc8bc8183feab5ba08e4146aca
Artem <temamak77@gmail.com>
2025-04-15 23:51:44 +0200

    KS-1-1 added variables and println

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main (){
+    var ordersQuantity: Int = 75
+    val appreciateText: String = "Thank you for your order!"
+
+    println(ordersQuantity)
+    println(appreciateText)
+
+    var employeeQuantity = 2000
+
+    //println(employeeQuantity)
+
+    employeeQuantity = 2000-1
+
+    println(employeeQuantity)
+}

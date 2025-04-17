be37b635dc0c8a0cd91c6a8c08230fea908fdda8
Artem <temamak77@gmail.com>
2025-04-17 21:36:04 +0200

    KS-2-1 average grade

---
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val firstGrade: Double = 3.0
+    val secondGrade: Double = 4.0
+    val thirdGrade: Double = 3.0
+    val fourthGrade: Double = 5.0
+    val average = (firstGrade + secondGrade + thirdGrade + fourthGrade) / 4
+    println(average)
+    //получилось, но я до конца не понимаю, почему получается 2 знака после запятой
+}

9a0d95c873205a856e9b04e6a25a51ad310204b3
Artem <temamak77@gmail.com>
2025-04-18 13:49:41 +0200

    KS-1-5 added constants and renamed vals

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,10 +1,12 @@
 package org.example.lesson_1
 
+const val SECONDS_IN_SPACE = 6480
+const val SECONDS_IN_MINUTE = 60
+const val MINUTES_IN_HOUR = 60
 fun main() {
-    val secondsCosmic = 6480
-    val minutesCosmic = secondsCosmic / 60
-    val hoursCosmic = minutesCosmic / 60
-    val minutesCosmicReminder = secondsCosmic % 60
-    val hoursCosmicReminder = minutesCosmic % 60
-    println("%02d:%02d:%02d".format(hoursCosmic, hoursCosmicReminder, minutesCosmicReminder))
+    val minutesInSpace = SECONDS_IN_SPACE / SECONDS_IN_MINUTE
+    val hoursInSpace = minutesInSpace / MINUTES_IN_HOUR
+    val minutesInSpaceReminder = SECONDS_IN_SPACE % SECONDS_IN_MINUTE
+    val hoursInSpaceReminder = minutesInSpace % MINUTES_IN_HOUR
+    println("%02d:%02d:%02d".format(hoursInSpace, hoursInSpaceReminder, minutesInSpaceReminder))
 }

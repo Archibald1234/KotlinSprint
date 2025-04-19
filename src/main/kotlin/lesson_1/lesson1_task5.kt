16064cca33c73ec6bdb92ea68d3295a61438a73f
Artem <temamak77@gmail.com>
2025-04-19 14:10:08 +0200

    KS-1-5 changed secondsInSpace from const to val

--- src/main/kotlin/lesson_1/lesson1_task5.kt
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,12 +1,13 @@
 package org.example.lesson_1
 
-const val SECONDS_IN_SPACE = 6480
+
 const val SECONDS_IN_MINUTE = 60
 const val MINUTES_IN_HOUR = 60
 fun main() {
-    val minutesInSpace = SECONDS_IN_SPACE / SECONDS_IN_MINUTE
+    val secondsInSpace = 6480
+    val minutesInSpace = secondsInSpace / SECONDS_IN_MINUTE
     val hoursInSpace = minutesInSpace / MINUTES_IN_HOUR
-    val minutesInSpaceReminder = SECONDS_IN_SPACE % SECONDS_IN_MINUTE
+    val minutesInSpaceReminder = secondsInSpace % SECONDS_IN_MINUTE
     val hoursInSpaceReminder = minutesInSpace % MINUTES_IN_HOUR
     println("%02d:%02d:%02d".format(hoursInSpace, hoursInSpaceReminder, minutesInSpaceReminder))
 }

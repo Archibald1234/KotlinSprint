2792a4db0f9ffe1ca290a30372ef1b0142219ca5
Artem <temamak77@gmail.com>
2025-04-19 14:29:04 +0200

    KS-2-4 changed percents math and const to val

--- src/main/kotlin/lesson_2/lesson2_task4.kt
+++ src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -5,7 +5,7 @@ fun main() {
     val chrystalOre = 7
     val ironOre = 11
     val buffPercentsQuantity = 20
-    val buff = buffPercentsQuantity/100.0
+    val buff = buffPercentsQuantity / 100.0
     val buffAdvantageCO = chrystalOre * buff
     val buffAdvantageIO = ironOre * buff
     println("Chrystal ore buff advantage:${buffAdvantageCO.toInt()}")
